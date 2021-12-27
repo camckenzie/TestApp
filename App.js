f38186273a9b2d5f3cef32aa57254d0724b84047
Christopher McKenzie <camckenzie94@gmail.com>
2021-12-27 17:09:18 -0500

    reverting fixes

--- App.js
+++ App.js
@@ -3,35 +3,21 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LoginScreen from "./screens/LoginScreen";
 import WelcomeScreen from "./screens/WelcomeScreen";
-import RegisterationScreen from "./screens/RegistrationScreen";
-//Below is new
-import {decode, encode} from 'base-64'
-if (!global.btoa) {  global.btoa = encode }
-if (!global.atob) { global.atob = decode }
+import SignupScreen from "./screens/SignupScreen";
 
 const Stack = createNativeStackNavigator();
 
 
 function App() {
-  
-  const [loading, setLoading] = useState(true)
-  const [user, setUser] = useState(null)
+
 
   return (
     <NavigationContainer>
       <Stack.Navigator
         screenOptions={{ headerShown: false }}>
-          {/* Below checks for user?? */}
-          { user ? (
-            <Stack.Screen name="Welcome">
-              {props => <WelcomeScreen {...props} extraData={user} />}
-            </Stack.Screen>
-          ): (
-            <>
-              <Stack.Screen name="Login" component={LoginScreen} />
-              <Stack.Screen name="Registration" component={RegistrationScreen} />
-            </>
-          )}
+        <Stack.Screen name="Signup" component={SignupScreen} />
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Welcome" component={WelcomeScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
