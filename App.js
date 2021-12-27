2c157a9ccbe84a7ec8181cc141f94756ae2aaa99
Christopher McKenzie <camckenzie94@gmail.com>
2021-12-27 17:22:13 -0500

    changes

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LoginScreen from "./screens/LoginScreen";
 import WelcomeScreen from "./screens/WelcomeScreen";
-import SignupScreen from "./screens/SignupScreen";
+//import RegistrationScreen from "./screens/RegistrationScreen";
 
 const Stack = createNativeStackNavigator();
 
@@ -15,8 +15,8 @@ function App() {
     <NavigationContainer>
       <Stack.Navigator
         screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="Signup" component={SignupScreen} />
         <Stack.Screen name="Login" component={LoginScreen} />
+        {/* <Stack.Screen name="Register" component={RegistrationScreen} /> */}
         <Stack.Screen name="Welcome" component={WelcomeScreen} />
       </Stack.Navigator>
     </NavigationContainer>
